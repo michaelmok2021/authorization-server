5e1aca727aceb8f73546befc9cdafbd30c5e9fa1
michael <moktc@hotmail.com>
2023-01-25 08:03:25 +0800

    Opaue token 1st attempt - Try to see if we can use opaque token to get the role instead client scope

--- backend-resources/src/main/java/com/sergio/auth/backend/resources/controllers/ResourceController.java
+++ backend-resources/src/main/java/com/sergio/auth/backend/resources/controllers/ResourceController.java
@@ -1,13 +1,26 @@
 package com.sergio.auth.backend.resources.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ResourceController {
 
+    private static final GrantedAuthority GOLD_CUSTOMER = new SimpleGrantedAuthority("gold");
+
+
     @GetMapping("/messages")
-    public String getMessages() {
+    public String getMessages(JwtAuthenticationToken auth ){
+        log.info("authorities is ", auth.getAuthorities());
+        if ( auth.getAuthorities().contains(GOLD_CUSTOMER)) {
+            // q.setPrice(10.0);
+        }
         return "the protected messages";
     }
 }
