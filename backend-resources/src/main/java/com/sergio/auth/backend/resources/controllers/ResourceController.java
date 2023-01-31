024a5d964de894039627f33848b8029e37b69d4a
Hamish <hamish@fastsoft.com.au>
2023-01-31 15:50:05 +0800

    Refining resources to use ResponseEntity and return JSON.

--- backend-resources/src/main/java/com/sergio/auth/backend/resources/controllers/ResourceController.java
+++ backend-resources/src/main/java/com/sergio/auth/backend/resources/controllers/ResourceController.java
@@ -1,15 +1,17 @@
 package com.sergio.auth.backend.resources.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.Hashtable;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @Slf4j
@@ -17,6 +19,8 @@ public class ResourceController {
 
     private static final GrantedAuthority GOLD_CUSTOMER = new SimpleGrantedAuthority("gold");
 
+    String[] elementArray = {"","Hydrogen","Helium","Lithium","Beryllium","Boron","Carbon","Nitrogen","Oxygen","Fluorine","Neon","Sodium","Magnesium","Aluminum","Silicon","Phosphorus","Sulfur","Chlorine","Argon","Potassium","Calcium","Scandium","Titanium","Vanadium","Chromium","Manganese","Iron","Cobalt","Nickel","Copper","Zinc","Gallium","Germanium","Arsenic","Selenium","Bromine","Krypton","Rubidium","Strontium","Yttrium","Zirconium","Niobium","Molybdenum","Technetium","Ruthenium","Rhodium","Palladium","Silver","Cadmium","Indium","Tin","Antimony","Tellurium","Iodine","Xenon","Cesium","Barium","Lanthanum","Cerium","Praseodymium","Neodymium","Promethium","Samarium","Europium","Gadolinium","Terbium","Dysprosium","Holmium","Erbium","Thulium","Ytterbium","Lutetium","Hafnium","Tantalum","Tungsten","Rhenium","Osmium","Iridium","Platinum","Gold","Mercury","Thallium","Lead","Bismuth","Polonium","Astatine","Radon","Francium","Radium","Actinium","Thorium","Protactinium","Uranium","Neptunium","Plutonium","Americium","Curium","Berkelium","Californium","Einsteinium","Fermium","Mendelevium","Nobelium","Lawrencium","Rutherfordium","Dubnium","Seaborgium","Bohrium","Hassium","Meitnerium","Darmstadtium","Roentgenium","","Ununbiium",""};
+    List<String> elements = new ArrayList<>(List.of(elementArray));
 
     @GetMapping("/messages")
 //    public String getMessages(JwtAuthenticationToken auth, @AuthenticationPrincipal Jwt princial){
@@ -46,5 +50,38 @@ public class ResourceController {
     public String rose(@AuthenticationPrincipal Jwt principal){
         return "--------{---(@";
     }
+    
+    @GetMapping(value = "/element/{num}")
+    public ResponseEntity<String> elementGet(@PathVariable String num) throws JsonProcessingException {
+        Map<String,String> result = new HashMap<>();
+        try {
+            result.put(num, elements.get(Integer.parseInt(num)));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return ResponseEntity.badRequest().build();
+        }
 
+        ObjectWriter ow = new ObjectMapper().writer();
+        String json = ow.writeValueAsString(result);
+        return ResponseEntity.ok(json);
+    }
+
+    @PostMapping(value = "/addElement")
+    public ResponseEntity<String> elementPost(@RequestBody String newElement) throws JsonProcessingException {
+        Map<String,String> result = new HashMap<>();
+        try {
+            String[] element = newElement.split(" ");
+            int atomicNum = Integer.parseInt(element[0]);
+
+            elements.set(atomicNum, element[1]);
+            result.put(element[0], elements.get(atomicNum));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return ResponseEntity.badRequest().build();
+        }
+
+        ObjectWriter ow = new ObjectMapper().writer();
+        String json = ow.writeValueAsString(result);
+        return ResponseEntity.ok(json);
+    }
 }
