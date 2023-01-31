55234e76f616415a2a6e43afe3f2722f3c4fd516
michaelmok2021 <michael.mok@fastsoft.com.au>
2023-01-31 11:16:29 +0800

    Try configuring oauth2 client programmically.

--- backend-gateway-client/src/main/java/com/sergio/auth/backend/client/config/SecurityConfig.java
+++ backend-gateway-client/src/main/java/com/sergio/auth/backend/client/config/SecurityConfig.java
@@ -9,6 +9,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -20,19 +27,32 @@ import java.util.Map;
 
 @Slf4j
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfig {
 
+    @Bean
+    ReactiveClientRegistrationRepository clientRegistrations() {
+        ClientRegistration clientRegistration = ClientRegistrations
+                .fromOidcIssuerLocation("https://sso.billview.com.au/realms/myfirsttest")
+                .clientId("homedemo1")
+                .clientSecret("Tazr42CrDbtuqS21zLNeQOcQaT3KyhYL")
+                .redirectUri("https://backend-gateway-client:8543/login/oauth2/code/{registrationId}")
+                .scope("openid", "email", "profile", "roles", "message.read")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .build();
+        return new InMemoryReactiveClientRegistrationRepository(clientRegistration);
+    }
+
     /**
-     * For the backend-resources, I indicate that all the endpoints are protected.
-     * To request any endpoint, the OAuth2 protocol is necessary, using the server configured and with the given scope.
-     * Thus, a JWT will be used to communicate between the backend-resources and backend-auth when backend-resources
-     * needs to validate the authentication of a request.
+     * https://docs.spring.io/spring-security/site/docs/5.1.5.RELEASE/reference/html/webflux-oauth2.html
+     * @param http
+     * @return
+     * @throws Exception
      */
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
         log.info("disabling csrf");
-        http.csrf(csrf -> csrf.disable());
+        http.csrf().disable()
+                .oauth2Client();
         return http.build();
     }
 
